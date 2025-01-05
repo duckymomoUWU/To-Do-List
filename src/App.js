20094dab7f1ab925e001854826e0da32ff4e377c
duckymomoUWU <168294168+duckymomoUWU@users.noreply.github.com>
2025-01-05 23:24:47 +0700

    update css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>My to do list</h1>
+      <div className="todo-container">
+        <h1>My to do lists</h1>
+        
+        <div className="todo-wrapper">
+          <div className="input-section">
+            <div className="todo-input-item">
+              <label>Title</label>
+              <input type="text" placeholder="ex: sport,.." />
+            </div>
+            
+            <div className="todo-input-item">
+              <label>Description</label>
+              <input type="text" placeholder="ex: play football,..." />
+            </div>
+            
+            <div className="todo-input-item">
+              <button className="primaryBt">Add</button>
+            </div>
+          </div>
+
+          <div className="controls-section">
+            <div className="btn-group">
+              <button className="secondBT">To do</button>
+              <button className="secondBT">Completed</button>
+            </div>
+          </div>
+
+          <div className="todo-list">
+            <div className="todo-item">
+              <div className="content">
+                <h3>Task title</h3>
+                <p>Task description</p>
+              </div>
+              <div className="btn-group">
+                <button className="primaryBt">Edit</button>
+                <button className="primaryBt">Delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
